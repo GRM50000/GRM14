ed51300bd38e20a23fed90de1a2c5fe7319adfed
GRM50000 <119069784+GRM50000@users.noreply.github.com>
2023-05-17 08:38:37 -0700

    FromGRM14
    first commit

---
+++ scripts/js1.js
@@ -0,0 +1,55 @@
+console.log('Gary Reid Backing Tracks');
+console.log('GRM50000')
+''
+console.log('Gary Reid Music')
+''
+
+//vvv
+//document.getElementById('ytVideo').src = "https://www.youtube.com/embed/TfIapBzeAos";
+
+document.getElementById('change-video-110').onclick = function () {
+    document.getElementById('ytVideo').src = 'https://www.youtube.com/embed/TfIapBzeAos';
+};
+
+
+document.getElementById('change-video-111').onclick = function () {
+    document.getElementById('ytVideo').src = 'https://www.youtube.com/embed/R9UfvtcmZwA';
+};
+
+document.getElementById("change-video-112").onclick = function () {
+    document.getElementById('ytVideo').src = "https://www.youtube.com/embed/o22v_4ARQE0";
+};
+
+
+document.getElementById("change-video-113").onclick = function () {
+    document.getElementById('ytVideo').src = "https://www.youtube.com/embed/o22v_4ARQE0";
+};
+document.getElementById("change-video-114").onclick = function () {
+    document.getElementById('ytVideo').src = "https://www.youtube.com/embed/o22v_4ARQE0";
+};
+
+//
+//{document.getElementById("change-video-11$").onclick = function() {document.getElementById('ytideo').src = "https://www.youtube.com/embed/11$";}}*9
+//
+
+//--
+
+//function myFunction() {
+//    let person = prompt("Please enter your name", "Harry Potter");
+//    if (person != null) {
+//        document.getElementById("header").innerHTML =
+//            "Welcome, " + person + "!";
+//        btn.style.display = 'none';
+//    }
+//
+//}
+
+//let input = localStorage.getItem('input')
+//
+//function returnText() {
+//    input = document.getElementById("userInput").value
+//    localStorage.setItem('input', input)
+//    alert(input)
+//}
+//
+//console.log(input)
