c437baa739d8281c6c0a38a2c5f709075c2b79e7
GRM50000 <119069784+GRM50000@users.noreply.github.com>
2023-05-22 07:54:30 -0700

    dark mode switch
    fark

--- scripts/jsDark.js
+++ scripts/jsDark.js
@@ -5,8 +5,8 @@ var icon = document.getElementById('icon');
 icon.onclick = function () {
   document.body.classList.toggle('dark-theme');
   if (document.body.classList.contains('dark-theme')) {
-    icon.src = "../../images/greenLight.png";
+    icon.src = "../images/greenLight.png";
   } else {
-    icon.src = "../../images/redDark.png";
+    icon.src = "../images/redDark.png";
   }
 }
